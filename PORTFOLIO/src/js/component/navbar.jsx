f2f182e694f0c16690e049cdaeb9a6e3aabcf83d
imaprettyprettyprettyprettyannoyingperson <clisha0811@gmail.com>
2026-07-17 17:56:17 -0700

    navbar added

---
+++ PORTFOLIO/src/js/component/navbar.jsx
@@ -0,0 +1,40 @@
+import { Link, NavLink } from "react-router-dom";
+import "../../css/components/navbar.css";
+
+function Navbar() {
+  const linkClass = ({ isActive }) =>
+    isActive ? "active" : "";
+
+  return (
+    <nav className="header__nav">
+      <div className="header__logo">
+        <Link to="/">
+          <h4>Name</h4>
+        </Link>
+      </div>
+
+      <ul className="header__menu" id="nav">
+        <li>
+          <NavLink to="/" className={linkClass}>
+            Home
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/about" className={linkClass}>
+            About
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/portfolio" className={linkClass}>
+            Portfolio
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
+
