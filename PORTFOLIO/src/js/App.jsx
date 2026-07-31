3ccbbceb8cb8737c64455366f6e37dc05e132d48
Joyce Lee <lee.joyce21@gmail.com>
2026-07-31 16:40:41 -0700

    Loading fix

--- PORTFOLIO/src/js/App.jsx
+++ PORTFOLIO/src/js/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './component/navbar';
+import Home from './pages/home';
+import About from './pages/about';
+import Portfolio from './pages/portfolio';
 
 function App() {
   return (
@@ -11,7 +14,7 @@ function App() {
         <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
     </BrowserRouter>
-    );
+  );
 }
 
 export default App;
---
+++ PORTFOLIO/src/js/pages/portfolio.jsx
@@ -0,0 +1,10 @@
+function Portfolio() {
+  return (
+    <section>
+      <h1>Portfolio</h1>
+      <p>My projects will appear here soon.</p>
+    </section>
+  );
+}
+
+export default Portfolio;
